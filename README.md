d13a0efc5e307bbf9e682eeb7b7f2d08317cb218
enrico alterani <e.alterani@gmail.com>
2016-09-26 22:26:37 +0200

    work

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # from-json-to-mongoDB
-This software is an utility for import json files into your database mongodb. It works in nodes environment.
+This software is an utility to import json files into your database mongodb. It works in nodes environment.
 
 #System requirements
 - nodejs
 - mongodb
+
+#Installation command
+<p>Only first time</p>
+<p><CODE>npm install</CODE></p>
 
 #Start command
 <p>To execute script type this command:</p>
@@ -15,11 +19,19 @@ This software is an utility for import json files into your database mongodb. It
 <p>In order to setup your parameters use json syntax.</p> 
 <p>Before to run check and configure your setting in this file.</p>
 
-Under you can see description for each parameters:
+Under, you can see description for each parameters:
 
-- <b>input_directory:</b> This is an loca path on your machine. Input directory path. Where your files json are stored.
+- <b>input_directory:</b> This is a local path on your machine. Input directory path. Where your files json are stored.
 
-- <b>input_directory:</b> This is an loca path on your machine. Input directory path. Where your files json are stored.
+- <b>dbhost:</b> Hostname or ip address where mongodb service is running.
+
+- <b>dbport:</b> Port number where mongodb service is running.
+
+- <b>dbname:</b> Database name.
+
+- <b>collectionname:</b> Collection Name.
+
+
 
 
 
--- json-to-mongo.js
+++ json-to-mongo.js
@@ -1,6 +1,91 @@
 // File json-to-mongo.js
 
 var parameters = require('./parameters.json');
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = "mongodb://" + parameters.dbhost + ":" +parameters.dbport + "/" + parameters.dbname;
+var stringafile;
+
+fs.readdir(parameters.input_directory,function(err, files){
+	
+	// Gestione errore
+	if (err) throw err;
+
+	// Look su elenco dei file
+	for(var i = 0; i < files.length; i++){
+		
+
+		if(files[i] != ".DS_Store")
+		{
+			console.log("");
+			console.log("Reading file " + files[i] + " ......");
+			console.log("");
+
+			var dati = fs.readFileSync(parameters.input_directory + '/'+ files[i],'utf8','r'); 
+			if (dati)
+			{
+				stringafile = stringafile + dati;
+		
+			} // fine if (dati)
+		} // fine if(files[i] != ".DS_Store")
+	
+	} // Fine for su nomi file in directory
+
+	var miocont = 0;
+	if(stringafile.length > 0)
+	{
+		while(miocont < stringafile.length && stringafile[miocont] != "{" ){
+			miocont++;
+		}
+		var len = stringafile.length; 
+		stringafile = stringafile.substring(miocont,len);	
+	}
+	
+	// se l'ultimo carattere è una , lo elimino
+	if(stringafile.length > 0 && stringafile[stringafile.length-1] == ",")
+	{
+		var len = stringafile.length; 
+		stringafile = stringafile.substring(0,len-1);
+	}
+
+	// se il file json non è contenuto in delle parentesi quadre le aggiunto.
+	if(stringafile.length > 0 && stringafile[0] != "[")
+	{
+		stringafile = "["+stringafile+"]";
+	}
+
+	if(stringafile.length > 0)
+	{
+		var array_lead = new Array;
+		array_lead= JSON.parse(stringafile);
+	
+	}
+	
+	// CONNESSIONE A DB
+	MongoClient.connect(url, function(err, db) 
+	{
+  		assert.equal(null, err);
+		insertDocuments(db, function() { db.close();}, array_lead); //end call insertDocuments
+	}); //end MongoClient.Connect
 
 
-console.log(parameters.input_directory);
+}); // fine fs.readdir
+
+//FUNZIONE INSERT IN DATABASE
+var insertDocuments = function(db, callback, mioarray) {
+  // Get the documents collection
+  
+  var collection = db.collection(parameters.collectionname);
+  // Insert some documents
+  collection.insertMany(mioarray, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(mioarray.length, result.result.n);
+    assert.equal(mioarray.length, result.ops.length);
+    console.log("Inserted "+ mioarray.length +" documents into the collection");
+    callback(result);
+  });
+}
+
+
+
